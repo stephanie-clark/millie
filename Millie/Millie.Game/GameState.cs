a488dff80685361d53a6d7d31a4b260e697446ef
Stephanie Clark <szuroff@gmail.com>
2016-03-09 21:29:00 -0500

    Added more main character attributes

--- Millie/Millie.Game/GameState.cs
+++ Millie/Millie.Game/GameState.cs
@@ -8,12 +8,18 @@ namespace Millie.Game
 {
     public class GameState
     {
-        public int CurrentRoomID { get; set; }
-        public int BeardLength { get; set; }    
-        public bool VisitedTattooRoom { get; set; }
-
-
-
+     
+        // character attributes 
+        public int BeardLength { get; set; }
+        public int NumberOfShotsTaken { get; set; }
         public bool GotTattoo { get; set; }
+        public bool HasFlowers { get; set; }
+
+        // visited locations  
+        public int CurrentRoomID { get; set; }
+        public bool VisitedTattooRoom { get; set; }
+        public bool VisitedBarCar { get; set; }
+        public bool VisitedLionTent { get; set; }
+
     }
 }
